5835495ee61edfeda95e223f3d54ff5221108678
thawatchai3 <thawatchai.mu@rmutsvmail.com>
2020-11-03 18:21:06 +0700

    Initial commit

---
+++ src/ExchangCurrencyApp.java
@@ -0,0 +1,19 @@
+public class ExchangCurrencyApp {
+
+    public static void main(String[] args) {
+
+        ExchangeAPI ex = new ExchangeAPI();
+
+        //test connection with THB
+        if (ex.getConnection("THB")){
+            System.out.println(ex.getResult());
+        }
+
+
+
+
+
+
+    }
+
+}
